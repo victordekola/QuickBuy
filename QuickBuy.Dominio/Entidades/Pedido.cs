e4680b7da067347da09aabbdf141ad2cd1e58eaa
antonio victor gomes tenorio <victordekola@gmail.com>
2020-08-27 15:29:16 -0300

    Criando as entidades e campos enums

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um itens de pedido ou muitos intes de pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItemPedido { get; set; }
+    }
+}
